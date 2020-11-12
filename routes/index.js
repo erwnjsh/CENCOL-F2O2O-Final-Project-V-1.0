c016ba8af0dfe03f1dc931cdf20ecd12718e921c
user <joshmanuel1996@gmail.com>
2020-11-11 19:38:33 -0500

    navbar functional - navbar active/inactive toggle not yet

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,26 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Home' });
+});
+
+/* GET home page. */
+router.get('/home', function(req, res, next) {
+  res.render('index', { title: 'Home' });
+});
+
+/* GET about page. */
+router.get('/about', function(req, res, next) {
+  res.render('about', { title: 'About' });
+});
+
+/* GET tutorSignUp page. */
+router.get('/TutorRegistration', function(req, res, next) {
+  res.render('regTutor', { title: 'Sign up to be a tutor' });
+});
+/* GET studentSignUp page. */
+router.get('/StudentRegistration', function(req, res, next) {
+  res.render('regStudent', { title: 'Sign up to be a tutor' });
 });
 
 module.exports = router;
